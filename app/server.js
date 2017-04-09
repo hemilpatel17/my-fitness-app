9b295b8789b98dfb4ec2e1c2bc8ddf328e6149b9
Hemil Patel <hemilpatel17@gmail.com>
2017-04-09 19:45:30 -0400

    finally

--- app/server.js
+++ app/server.js
@@ -1,9 +1,13 @@
-
-var express = require('express');
+var express = require('express')
 var app = express()
+var path = require('path')
 
-app.get('/index.html', function(req, res) {
-	//res.send("Hello World!!!");
-});
+// viewed at http://localhost:8080
+//app.use('./public', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '..', 'public')))
 
-app.listen(3000);
+app.get('/', function(req, res) {
+    res.sendFile('index.html');
+})
+
+app.listen(3000)
