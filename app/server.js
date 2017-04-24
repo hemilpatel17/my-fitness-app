a1300652035e8c7b561d5d53c4ce8f429cdbb903
Hemil Patel <hemilpatel17@gmail.com>
2017-04-23 22:45:01 -0400

    trying out todo app

--- app/server.js
+++ app/server.js
@@ -2,12 +2,13 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
-// viewed at http://localhost:8080
-//app.use('./public', express.static(path.join(__dirname, 'public')))
+//lets your use / as static dir
 app.use('/', express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', function(req, res) {
+  res.sendFile('index.css');
     res.sendFile('index.html');
+
 })
 
 app.listen(3000)
